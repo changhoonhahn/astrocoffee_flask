9e0fdfd6102799bd4ed0bbb6e49ed4aeaf8f7c5e
ChangHoon Hahn <chh327@nyu.edu>
2017-06-03 18:25:49 -0400

    astrocoffee website

---
+++ arxiv.py
@@ -0,0 +1,56 @@
+'''
+
+Testing arXiv's API and parsing the strings 
+
+
+'''
+from random import shuffle
+
+import urllib2
+from bs4 import BeautifulSoup 
+
+
+def scrapeNewFeed(): 
+    '''
+    '''
+    page = urllib2.urlopen('https://arxiv.org/list/astro-ph/new/') 
+    soup = BeautifulSoup(page, "html.parser")
+
+    urls = []
+    for dt in soup.find_all('dt'): 
+        urls.append(''.join(['https://arxiv.org', dt.span.a['href']]))
+    
+    arxiv_entries = [] 
+    for i_dd, dd in enumerate(soup.find_all('dd')): 
+        arxiv_entry = {} 
+        
+        for attr in ['title', 'comments', 'subjects']: 
+            if attr == 'title': 
+                attr_name = 'title mathjax'
+            else: 
+                attr_name = attr 
+
+            for div in dd.find_all('div', "list-"+attr_name): 
+                arxiv_entry[attr] = div.prettify().split('</span>')[-1].split('</div>')[0]
+    
+        # authors
+        arxiv_entry['authors'] = [] 
+        for div in dd.find_all('div', "list-authors"):
+            for auth in div.find_all('a'): 
+                arxiv_entry['authors'].append(auth.string)  
+        arxiv_entry['authors_str'] = ', '.join(arxiv_entry['authors'])
+        # abstract 
+        try: 
+            arxiv_entry['abstract'] = unicode(dd.p.string)
+        except AttributeError: 
+            arxiv_entry['abstract'] = '' 
+        arxiv_entry['url'] = urls[i_dd]
+
+        arxiv_entries.append(arxiv_entry)
+   
+    assert len(urls) == len(arxiv_entries)
+    shuffle(arxiv_entries)
+    return arxiv_entries
+
+if __name__=='__main__': 
+    scrapeNewFeed()
---
+++ astrocoffee.py
@@ -0,0 +1,46 @@
+from flask import Flask, render_template
+from flask import request, redirect 
+from flask import session
+
+from arxiv import scrapeNewFeed
+
+app = Flask(__name__)
+
+@app.route('/')
+def astrocoffee():
+    author = "ChangHoon Hahn"
+    return render_template('index.html')#, author=author, name=name)
+
+#@app.route('/signup', methods = ['POST'])
+#def signup(): 
+#    email = request.form['email']
+#    email_addresses.append(email)
+#    session['email'] = email
+#    print(email_addresses)
+#    return redirect('/')
+
+#@app.route('/emails.html')
+#def emails():
+        #    return render_template('emails.html', email_addresses=email_addresses)
+
+@app.route('/arxiv.html')
+def arxiv():
+    entries = scrapeNewFeed()
+    return render_template('arxiv.html', entries=entries) 
+
+#@app.route('/unregister')
+#def unregister():
+#    # Make sure they've already registered an email address
+#    if 'email' not in session:
+#        return "You haven't submitted an email!"
+#    email = session['email']
+#    # Make sure it was already in our address list
+#    if email not in email_addresses:
+#        return "That address isn't on our list"
+#    email_addresses.remove(email)
+#    del session['email'] # Make sure to remove it from the session
+#    return 'We have removed ' + email + ' from the list!'
+
+
+if __name__ == '__main__':
+    app.run()
